a74126b9a52d85e55701f08638d0ac17c59da19e
Joseph Abraham <ekurubal24@gmail.com>
2021-02-12 22:52:58 +0300

    Add formik

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,51 +1,90 @@
 import React from 'react';
 import { Button, TextField, Grid, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useFormik } from "formik";
+
+const styles = makeStyles({
+    wrapper: {
+        marginTop: "3rem",
+        height: "calc(100vh - 19.0625rem)",
+        textAlign: "center",
+        marginBottom: "12rem",
+    },
+  });
 
 const Signup = () => {
+    const formik = useFormik({
+        initialValues: {
+            username:"",
+            email: '',
+            password:"",
+            password2:"",
+        },
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
+
+    const signupStyles = styles();
+    console.log(formik)
     return(
-        <Container maxWidth="sm">
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <TextField 
-                        name="username"
-                        label="Username"
-                        variant="outlined" 
-                        fullWidth 
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField 
-                        name="email" 
-                        label="Email" 
-                        variant="outlined" 
+        <Container className={signupStyles.wrapper} maxWidth="sm">
+            <form onSubmit={formik.handleSubmit}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="username"
+                            label="Username"
+                            variant="outlined" 
+                            fullWidth
+                            onChange={formik.handleChange}
+                            value={formik.values.username}
+  
+                            />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="email" 
+                            label="Email" 
+                            variant="outlined" 
+                            fullWidth
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="password" 
+                            label="Password" 
+                            variant="outlined" 
+                            fullWidth
+                            type="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <TextField
+                        name="password2"
+                        label="Password Again"
+                        variant="outlined"
+                        type="password"
                         fullWidth
-                    />
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button 
+                            type="submit" 
+                            variant="contained" 
+                            color="primary" 
+                            fullWidth>
+                                Register
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField 
-                        name="password" 
-                        label="Password" 
-                        variant="outlined" 
-                        fullWidth/>
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    name="password2"
-                    label="Password Again"
-                    variant="outlined"
-                    type="password"
-                    fullWidth
-                />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button 
-                        type="submit" 
-                        variant="contained" 
-                        color="primary" 
-                        fullWidth>Register
-                    </Button>
-                </Grid>
-            </Grid>
+            </form>
         </Container>
     )
 }
