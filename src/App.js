cae0ed44254ad8d9bee7c77b41e6d7bf789c0a18
Joseph Abraham <ekurubal24@gmail.com>
2021-02-15 22:32:29 +0300

    Added context

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Navbar from './components/Navbar';
+import AuthContextProvider from "./context/AuthContext";
 
 function App() {
   return (
-    <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/register" component={Signup}/>
-          <Route path="/login" component={Signin}/>
-        </Switch>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/register" component={Signup}/>
+            <Route path="/login" component={Signin}/>
+          </Switch>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
